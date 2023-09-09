a60aca77e83481e460489b8372bf62041441110c
Michael <fidd.michael@gmail.com>
2023-09-09 11:29:29 +0200

    hooks refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,36 @@ import { AddContactForm } from "./AddContactForm/AddContactForm";
 import { ContactsFilter } from "./ContactsFilter/ContactsFilter";
 import { Section } from "./Section.styled";
 
-const KEY = "CONTACTS_ARR";
+const KEY = "localContacts";
+
+
+
+const initialContacts = [
+  {id: 'id-1', name: 'Rosie Simpson', tel: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', tel: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', tel: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', tel: '227-91-26'}
+];
+
+const getInitialContacts = () => {
+  const localContacts = localStorage.getItem(KEY)
+    if(localContacts !== null){
+      return JSON.parse(localContacts)
+    } 
+    return initialContacts
+}
 
 export const App = () => {
 
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(getInitialContacts);
   const [filter, setFilter] = useState('');
-  const [haveContacts, setHaveContacts] = useState(false);
-
-  const localContacts = localStorage.getItem(KEY)
+  const [haveContacts, setHaveContacts] = useState(true);
 
   const addContact = (newContact) => {
     if(contacts.find(contact => contact.name === newContact.name)){
       alert(`${newContact.name} is already in your phonebook`);
       return;
     }
-
     setContacts(prevState => [...prevState, newContact])
   }
 
@@ -31,31 +45,16 @@ export const App = () => {
     setFilter(contactName)
     return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase))
   }
-  
-  // get contacts from Local Storage
-  useEffect(() => {
-    setContacts(() => {
-      if(localContacts !== null){
-        setContacts(JSON.parse(localContacts))
-      }
-    })
-    if(contacts.length === 0){
-      setHaveContacts(false);
-    } else{
-      setHaveContacts(true);
-    }
-  }, [localContacts, contacts])
-
   // put contacts to local storage
-  
-  useEffect((prevState)=> {
-    if(prevState !== contacts){
+  useEffect(()=> {
       localStorage.setItem(KEY, JSON.stringify(contacts))
-    }
+      if(contacts.length === 0){
+        setHaveContacts(false)
+      }
+      if(contacts.length !== 0){
+        setHaveContacts(true)
+      }
   }, [contacts])
-
-
-
 
   return(
     <>
